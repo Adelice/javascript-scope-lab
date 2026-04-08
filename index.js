bb95f3132fb58bd2d75e73514a960026380aadb6
Nancy Adelice Umutoniwase <nancytoniw@gmail.com>
2026-04-08 19:11:57 +0200

    Changes applied

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const burgers =['Hamburger','Cheeseburger'];
+let featuredDrink='Strawberry Milkshake';
+
+function addBurger(){
+	const newBurger='Flatburger';
+	burgers.push(newBurger);
+	// console.log(burgers)
+}
+addBurger();
